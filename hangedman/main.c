977ca2cb89a9f241e6c2994f703ee0164c321b29
ishmum14 <faieqdaiyanishmum@gmail.com>
2024-10-26 03:35:04 +0600

    Update 1 main.c

--- hangedman/main.c
+++ hangedman/main.c
@@ -3,6 +3,28 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("Welcome to Hangedman!\n");
+
+    char guess;
+    int attemptsleft=6;
+    char word='a';
+    while (attemptsleft > 0){
+        printf("enter your guess: ");
+        scanf(" %c",&guess);
+
+        if (guess == word) {
+            printf("correct\n");
+            return 0;
+        }
+         else{
+            printf("incorrect\n");
+        }
+
+        attemptsleft--;
+        printf("attempts remaining: %d\n", attemptsleft);
+    }
+
+
+
     return 0;
 }
