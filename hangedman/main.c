ef4873546a25ed3f492c55b63364ed9949e14ca7
ishmum14 <faieqdaiyanishmum@gmail.com>
2024-11-24 23:59:23 +0600

    Update main.c

--- hangedman/main.c
+++ hangedman/main.c
@@ -8,66 +8,66 @@
 void drawHangman(int attemptsleft) {
     switch (attemptsleft) {
         case 6:
-            printf(" +----+ \n");
-            printf(" |    | \n");
-            printf("      | \n");
-            printf("      | \n");
-            printf("      | \n");
-            printf("      | \n");
+            printf("  +-----+ \n");
+            printf("  |     | \n");
+            printf("        | \n");
+            printf("        | \n");
+            printf("        | \n");
+            printf("        | \n");
             printf(" ========= \n");
             break;
         case 5:
-            printf(" +----+ \n");
-            printf(" |    | \n");
-            printf(" O    | \n");
-            printf("      | \n");
-            printf("      | \n");
-            printf("      | \n");
+            printf("  +-----+ \n");
+            printf("  |     | \n");
+            printf("  O     | \n");
+            printf("        | \n");
+            printf("        | \n");
+            printf("        | \n");
             printf(" ========= \n");
             break;
         case 4:
-            printf("  +---+ \n");
-            printf("  |   | \n");
-            printf("  O   | \n");
-            printf("  |   | \n");
-            printf("      | \n");
-            printf("      | \n");
+            printf("  +-----+ \n");
+            printf("  |     | \n");
+            printf("  O     | \n");
+            printf("  |     | \n");
+            printf("        | \n");
+            printf("        | \n");
             printf(" ========= \n");
             break;
         case 3:
-            printf(" +----+ \n");
-            printf(" |    | \n");
-            printf(" O    | \n");
-            printf(" /|   | \n");
-            printf("      | \n");
-            printf("      | \n");
+            printf("  +-----+ \n");
+            printf("  |     | \n");
+            printf("  O     | \n");
+            printf(" /|     | \n");
+            printf("        | \n");
+            printf("        | \n");
             printf(" ========= \n");
             break;
         case 2:
-            printf(" +----+ \n");
-            printf(" |    | \n");
-            printf(" O    | \n");
-            printf(" /|\\ | \n");
-            printf("      | \n");
-            printf("      | \n");
+            printf("  +-----+ \n");
+            printf("  |     | \n");
+            printf("  O     | \n");
+            printf(" /|\\    | \n");
+            printf("        | \n");
+            printf("        | \n");
             printf(" ========= \n");
             break;
         case 1:
-            printf(" +-----+ \n");
-            printf(" |     | \n");
-            printf(" O     | \n");
-            printf(" /|\\  | \n");
-            printf(" /     | \n");
-            printf("       | \n");
+            printf("  +-----+ \n");
+            printf("  |     | \n");
+            printf("  O     | \n");
+            printf(" /|\\    | \n");
+            printf(" /      | \n");
+            printf("        | \n");
             printf(" ========= \n");
             break;
         case 0:
-            printf(" +-----+ \n");
-            printf(" |     | \n");
-            printf(" O     | \n");
-            printf(" /|\\  | \n");
-            printf(" / \\  | \n");
-            printf("       | \n");
+            printf("  +-----+ \n");
+            printf("  |     | \n");
+            printf("  O     | \n");
+            printf(" /|\\    | \n");
+            printf(" / \\    | \n");
+            printf("        | \n");
             printf(" ========= \n");
             break;
     }
